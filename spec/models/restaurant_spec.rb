3e141f793d66a7360eca15a6a737b3db13fa90e9
Tristan Crossley <trcrossley@gmail.com>
2016-01-19 10:16:15 +0000

    Added review validation and restaurant uniqueness

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
+
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
+  end
+
+  it 'it not valid unless it has a unique name' do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
   end
 end
